3496f99b75b365b88cc8a35bf2867bb784eefda1
Joseph Ensminger <josephensminger@Josephs-MacBook-Pro.local>
2015-10-08 15:01:02 -0500

    Subtraction function added and will return error if resulting in negative currency

--- currency.rb
+++ currency.rb
@@ -28,10 +28,25 @@ class Currency
     end
   end
 
+  
+  def - obj2
+    if self.currency_id == obj2.currency_id
+      if (self.amount - obj2.amount >= 0) && (obj2.amount - self.amount >= 0)
+        return Currency.new(currency_id,self.amount - obj2.amount)
+      else
+        "You can not have negative currency."
+      end
+    else
+      "Two currency's are not the same."
+    end
+  end
+  
+
 end
 
-puts currency1 = Currency.new("AUD", 1)
+puts currency1 = Currency.new("US", 1)
 puts currency2 = Currency.new("US", 0.23)
 puts currency1.currency_id == currency2.currency_id
-new_currency = currency1 + currency2
+new_currency = currency2 - currency1
+
 puts new_currency
