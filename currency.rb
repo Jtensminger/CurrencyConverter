0aca35898267fa230d6939b5edffe64a018b534a
Joseph Ensminger <josephensminger@Josephs-MacBook-Pro.local>
2015-10-08 13:18:28 -0500

    Setup currency.rb init funcion

--- currency.rb
+++ currency.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 class Currency
+  
+  attr_accessor :currency_id, :amount
+
+  def initialize currency_id, amount
+    @currency_id = currency_id
+    @amount = amount
+  end
   
 end
